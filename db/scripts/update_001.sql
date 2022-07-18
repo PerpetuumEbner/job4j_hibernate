2ccabc7b0c65e172d60f13c7e0bb25bc967324f4
Stanislav <yustas@protonmail.com>
2022-07-18 13:29:53 +0500

    3.4.0. Что такое интеграционное тестирование.

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,31 +1,40 @@
-CREATE TABLE candidates
-(
-    id         SERIAL PRIMARY KEY,
-    name       TEXT,
-    experience TEXT,
-    salary     INTEGER
-);
+-- CREATE TABLE candidates
+-- (
+--     id         SERIAL PRIMARY KEY,
+--     name       TEXT,
+--     experience TEXT,
+--     salary     INTEGER
+-- );
+--
+-- CREATE TABLE brands
+-- (
+--     id   SERIAL PRIMARY KEY,
+--     name TEXT UNIQUE
+-- );
+--
+-- CREATE TABLE models
+-- (
+--     id   SERIAL PRIMARY KEY,
+--     name TEXT UNIQUE
+-- );
+--
+-- CREATE TABLE authors
+-- (
+--     id   SERIAL PRIMARY KEY,
+--     name TEXT UNIQUE
+-- );
+--
+-- CREATE TABLE books
+-- (
+--     id   SERIAL PRIMARY KEY,
+--     name TEXT UNIQUE
+-- );
 
-CREATE TABLE brands
+CREATE TABLE orders
 (
-    id   SERIAL PRIMARY KEY,
-    name TEXT UNIQUE
-);
-
-CREATE TABLE models
-(
-    id   SERIAL PRIMARY KEY,
-    name TEXT UNIQUE
-);
-
-CREATE TABLE authors
-(
-    id   SERIAL PRIMARY KEY,
-    name TEXT UNIQUE
-);
-
-CREATE TABLE books
-(
-    id   SERIAL PRIMARY KEY,
-    name TEXT UNIQUE
+    id          serial,
+    name        VARCHAR(50),
+    description VARCHAR(50),
+    created     timestamp,
+    PRIMARY KEY (id)
 )
